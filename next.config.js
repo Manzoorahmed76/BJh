d36d5da7ea60c9a57116592fdc0a27acea5766b0
Don't open it <160379580+Manzoorahmed76@users.noreply.github.com>
2025-12-25 10:56:50 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        // Jab koi b-jh.vercel.app/f/file-name par jaye
         source: '/f/:path*',
+        // To asliyat mein yahan se file uthaye
         destination: 'https://98xavqwq9nylujsp.public.blob.vercel-storage.com/:path*',
       },
     ];
